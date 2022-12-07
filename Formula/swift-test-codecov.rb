ccd9f9ce73c377d879aa9048ca0c48fd85fbf1ef
Eric DeLabar <edelabar@gmail.com>
2022-12-07 15:22:50 -0500

    Add custom formulae for swift-test-codecov

---
+++ Formula/swift-test-codecov.rb
@@ -0,0 +1,13 @@
+class SwiftTestCodecov < Formula
+  desc "Swift test --enable-code-coverage result parser for use in CI/CD workflows"
+  homepage "https://github.com/mattpolzin/swift-test-codecov"
+  url "https://github.com/edelabar/swift-test-codecov/archive/refs/tags/0.11.3.tar.gz"
+  sha256 "64ca845430b2995ae0f788b85ffd0279e233b05a14592acad48ef6216f54d43d"
+  head "https://github.com/edelabar/swift-test-codecov.git"
+
+  depends_on xcode: ["14.1", :build]
+
+  def install
+    system "make", "install", "prefix=#{prefix}"
+  end
+end
