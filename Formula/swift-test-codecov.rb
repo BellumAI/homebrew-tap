2957423b91a884a0eae9907259276d8a061c04e7
Eric DeLabar <edelabar@gmail.com>
2022-12-08 09:23:20 -0500

    update swift-test-codecov to use owner's repo

--- Formula/swift-test-codecov.rb
+++ Formula/swift-test-codecov.rb
@@ -1,10 +1,10 @@
 class SwiftTestCodecov < Formula
   desc "Swift test --enable-code-coverage result parser for use in CI/CD workflows"
   homepage "https://github.com/mattpolzin/swift-test-codecov"
-  url "https://github.com/edelabar/swift-test-codecov/archive/refs/tags/0.11.3.tar.gz"
-  sha256 "64ca845430b2995ae0f788b85ffd0279e233b05a14592acad48ef6216f54d43d"
-  head "https://github.com/edelabar/swift-test-codecov.git"
-
+  url "https://github.com/mattpolzin/swift-test-codecov/archive/refs/tags/0.11.3.tar.gz"
+  sha256 "4d258c47f7038ce4735d288eeb014c954b8dee7ab57274323f04e37c9e3988bc"
+  head "https://github.com/mattpolzin/swift-test-codecov.git"
+  
   def install
     system "make", "install", "prefix=#{prefix}"
   end
