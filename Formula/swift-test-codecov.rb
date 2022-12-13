5bb8e2176d0984a33cd80cca534eaf86daf8df9e
Eric DeLabar <edelabar@gmail.com>
2022-12-13 09:15:40 -0500

    bump version fro swift-test-codecov

--- Formula/swift-test-codecov.rb
+++ Formula/swift-test-codecov.rb
@@ -2,8 +2,8 @@ class SwiftTestCodecov < Formula
   desc "Simple executable to produce code coverage numbers from the output of `swift test --enable-code-coverage`"
   homepage "https://github.com/mattpolzin/swift-test-codecov"
   url "https://github.com/edelabar/swift-test-codecov.git",
-      tag:      "0.11.4-beta3",
-      revision: "92563ef765bd879020d143846ee28b2c3e5a242d"
+      tag:      "0.11.4-beta4",
+      revision: "3432734454cb68bc0d85f39b2b8ed39e91ebf22a"
   head "https://github.com/edelabar/swift-test-codecov.git"
   license "MIT"
 
